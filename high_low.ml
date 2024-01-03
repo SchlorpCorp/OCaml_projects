2a51b16595dd300a2d88b417edc0ba9c445d02a0
SchlorpCorp <154564175+SchlorpCorp@users.noreply.github.com>
2024-01-03 13:41:35 -0700

    Add files via upload

---
+++ high_low.ml
@@ -0,0 +1,15 @@
+print_string "Player 1, enter your number: "
+
+let num = read_int()
+
+let rec high_low n m =
+  let _ = print_string "Player 2, enter your guess: " in
+  let i = read_int () in
+    if i = n then print_endline ("Spot on; total number of guesses was: " ^ (string_of_int m))
+    else
+      let () =  
+        if i < n then print_endline ("Too low; total number of guesses so far is: " ^ (string_of_int m))
+        else print_endline ("Too high; total number of guesses so far is: " ^ (string_of_int m))
+      in high_low n (m + 1)
+
+let _ = high_low num 1
